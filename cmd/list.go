f2c3bb5f743f0cdcfb597f29321a5fc66ce59597
Lee Wei <leewei@17.media>
2021-04-12 21:42:32 +0800

    validate input

--- cmd/list.go
+++ cmd/list.go
@@ -22,24 +22,36 @@ THE SOFTWARE.
 package cmd
 
 import (
-	c "github.com/leewei05/doge/common"
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
-// addCmd represents the add command
-var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "add a TODO to TODO list",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		err := c.IsValidArgs(args)
-		if err != nil {
-			return err
-		}
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
 
-		return nil
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("list called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(addCmd)
+	rootCmd.AddCommand(listCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
