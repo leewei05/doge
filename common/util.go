9e3c5a0496f3c31a88bcc2a078fc0c4e8f8bdfb8
Lee Wei <leewei@17.media>
2021-04-14 22:22:18 +0800

    add currentdate util

--- common/util.go
+++ common/util.go
@@ -1,9 +1,16 @@
 package common
 
 import (
+	"os"
 	"time"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+var (
+	todoFile = "todo.txt"
 )
 
 func IsValidArgs(args []string) error {
@@ -14,6 +21,32 @@ func IsValidArgs(args []string) error {
 	return nil
 }
 
-func CurrentDate() string {
+func currentDate() string {
 	return time.Now().Format("2006-01-02")
+}
+
+func Path2Todo() (s string, err error) {
+	home, err := homedir.Dir()
+	if err != nil {
+		return "", err
+	}
+
+	// refactor with string join
+	pathTodo := home + "/.doge/" + currentDate() + "/" + todoFile
+
+	return pathTodo, nil
+}
+
+func CheckTodoDir() {
+	home, err := homedir.Dir()
+	cobra.CheckErr(err)
+
+	dogeDir := home + "/.doge/" + currentDate() + "/"
+
+	if _, err := os.Stat(dogeDir); os.IsNotExist(err) {
+		err := os.Mkdir(dogeDir, 0755)
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+	}
 }
